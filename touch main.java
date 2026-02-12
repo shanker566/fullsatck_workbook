f43278aba8f0315dfd347441fff41be63cc45855
shanker566 <gowrishankerbhonagiri56@gmail.com>
2026-02-12 14:32:11 +0530

    Initial commit with main.java and notes.txt

---
+++ touch main.java
@@ -0,0 +1,17 @@
+// main.java
+// Simple Java Program for Git Demo
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Git Version Control Demo");
+        System.out.println("This file is tracked using Git.");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum of a and b is: " + sum);
+    }
+}
